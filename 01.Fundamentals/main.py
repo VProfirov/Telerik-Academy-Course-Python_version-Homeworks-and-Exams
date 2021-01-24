71c668cf3af0ac45dd7660bdd8526e510ba8e226
Vasil Profirov <Vasil.Profirov@gmail.com>
2021-01-24 21:38:31 +0200

    01.Fundamentals => 01.Introduction-to-Programming => Python
    Project structure,staging and figuring out relative import patterns (...next packaging)

---
+++ 01.Fundamentals/main.py
@@ -0,0 +1,20 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+# from _01_Introduction-to-Programming/_04_HelloWorld.py import hello_world()
+
+# _01_Introduction-to-Programming/_04_HelloWorld.py
+# from _01_Introducition-to-Programmind._04_HelloWorld import hello_world
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
