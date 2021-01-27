5780bc74ff2b9161916464aa198a04d57b8be0e1
Vasil Profirov <Vasil.Profirov@gmail.com>
2021-01-27 21:56:58 +0200

    01.01.00

---
+++ 01.Fundamentals/_01_Introduction-to-Programming/_00_Python-language-exploration.py
@@ -0,0 +1,18 @@
+multilineVar = """line1
+line2
+line3"""
+print(multilineVar)
+"""
+This seems to be a multi
+line comment since it is literal string not assigned to any variableiable
+and will be skipped by the interpreter
+"""
+print("hi...")
+
+# someInput_withPrompt = input("Insert and integer: ")
+# print(someInput_withPrompt + "of type: " + type(someInput_withPrompt))
+variable = 5
+print(str(variable) + " of type: " + str(type(variable)))
+print(type(variable))
+print(variable + variable)
+print("some str", "some string literal", sep=" ")
