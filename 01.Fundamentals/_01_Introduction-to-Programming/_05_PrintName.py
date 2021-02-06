5de41e955359caa8eea15d6574e638c126ff2ed8
Vasil Profirov <Vasil.Profirov@gmail.com>
2021-02-06 13:08:31 +0200

    01.01.111

--- 01.Fundamentals/_01_Introduction-to-Programming/_05_PrintName.py
+++ 01.Fundamentals/_01_Introduction-to-Programming/_05_PrintName.py
@@ -3,8 +3,10 @@ def print_name(name):
 
 
 if __name__ == '__main__':
+    # defaulting name:
     name = 'Pesho Peshev'
     input_user_name = input('Enter user name: ')
     if input_user_name:
         name = input_user_name
+
     print_name(name)
---
+++ _00_Generic-Python-Research_misc/Python_concepts/special_variables/sp_var.py
@@ -0,0 +1,25 @@
+from tst
+
+
+def __init__(self, *args, **kwargs):
+    super(CLASS_NAME, self).__init__(*args, **kwargs)
+
+
+# __doc__
+# THE ENTIRE ARTICLE at : http://www.exampleprogramming.com/specialvars.html
+
+# FIXME: python code style FIX extentions/app
+
+'''
+▶/usr/bin/python /home/'$USER'/.vscode/extensions/ms-python.python-2021.1.502429796/pythonFiles/pyvsc-run-isolated.py pip install -U autopep8 --user
+Collecting autopep8
+  Using cached autopep8-1.5.5-py2.py3-none-any.whl (44 kB)
+Requirement already satisfied, skipping upgrade: toml in /home/'$USER'/.local/lib/python3.9/site-packages (from autopep8) (0.10.2)
+Collecting pycodestyle>=2.6.0
+  Using cached pycodestyle-2.6.0-py2.py3-none-any.whl (41 kB)
+Installing collected packages: pycodestyle, autopep8
+  WARNING: The script pycodestyle is installed in '/home/'$USER'/.local/bin' which is not on PATH.
+  Consider adding this directory to PATH or, if you prefer to suppress this warning, use --no-warn-script-location.
+  WARNING: The script autopep8 is installed in '/home/'$USER'/.local/bin' which is not on PATH.
+  Consider adding this directory to PATH or, if you prefer 
+'''
