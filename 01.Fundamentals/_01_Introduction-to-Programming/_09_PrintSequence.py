d8dadce0a3084d466d522292038ef1ff3b876891
Vasil Profirov <Vasil.Profirov@gmail.com>
2021-08-24 22:36:08 +0300

    01.01-fix

--- 01.Fundamentals/_01_Introduction-to-Programming/_09_PrintSequence.py
+++ 01.Fundamentals/_01_Introduction-to-Programming/_09_PrintSequence.py
@@ -5,6 +5,8 @@ def build_sequence(seq_size):
     seq = ""
     upper_bound = int(start + seq_size)
     seq += str(start)
+    # method zero size check (vs input check)
+    if (seq_size == 0): return "";
     for step in range(start + 1, upper_bound):
         if (step & 1):  # odd
             seq += ", " + str(step * - 1)
@@ -17,6 +19,6 @@ def build_sequence(seq_size):
 if __name__ == '__main__':
     seq_size = 10
     user_input_seq_size = int(math.fabs(int(input("Enter sequence size(defaults for sequence size of 10): ") or 0)))
+    # input zero-size check
     if user_input_seq_size > 0: seq_size = user_input_seq_size
     print(build_sequence(seq_size))
-    print('hi')
